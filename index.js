00037a1caf1dacfb60819f447755089b23ea83af
Thomas Hinders <thomas.hinders@hotmail.com>
2024-09-18 15:32:20 -0300

    start script

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -13,6 +13,6 @@ app.get('/', (req, res) => {
 
 app.use(express.static('public'));
 
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
 });
